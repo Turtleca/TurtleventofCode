8dee8b7e3ebc6ed1a70e5d3c081d05493a4323e6
Turtle.ca <sleepingturtle294@gmail.com>
2022-12-01 23:19:17 -0700

    Doesnt work but it can read the input

---
+++ Day 2/Day2-1.py
@@ -0,0 +1,34 @@
+# WIN AT ROCK PAPER SCISSORS
+
+#Point scores are
+# 1 for Rock A X
+# 2 for paper B Y
+# 3 for scissors C Z
+
+# 0 for loss 
+# 3 for draw
+# 6 for win
+
+NEWLINE = "\n"
+SPACE = " "
+
+def RPS():
+    filename = "C:/Users/sleep/OneDrive/Documents/PythonPrograms/ADventofCode/inputs/day2-1.txt"
+    openFile = open(filename, "r")
+
+    totalScore = 0
+    LOSS = 0
+    DRAW = 3
+    WIN = 6
+    ROCK = "A","X"
+    PAPER = "B","Y"
+    SCISSORS = "C","Z"
+
+    for line in openFile:
+
+    print(totalScore)
+
+        
+        
+
+RPS()
