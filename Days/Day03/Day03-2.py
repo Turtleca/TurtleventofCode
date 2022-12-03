7df863fad49ff49de55a0a5ed3b7ba0303aa56d8
Turtle.ca <sleepingturtle294@gmail.com>
2022-12-03 00:56:06 -0700

    gotta sleep off this high

--- Days/Day03/Day03-2.py
+++ Days/Day03/Day03-2.py
@@ -4,6 +4,8 @@
 # sum all up at the end
 
 # part 2 compare every 3 lines
+#TODO be able to run more than one line or at ;east remember every 3
+
 def run(openFile):
     totalScore = 0
     for line in openFile:
@@ -11,21 +13,16 @@ def run(openFile):
         line = line.strip("\n")
         length = len(line)
         lineList = [line[:length//2],line[length//2:]]
-
-        #add 2 more list cases?
-        
-
         lineChecked = False
-        
         #Now to check for the same character in a string
         for char1 in lineList[0]:
             for char2 in lineList[1]:
                 if char1 == char2 and lineChecked == False:
                     if ord(char1) < 97:
                         totalScore += (ord(char1) - 38)
-                        lineChecked = True
                     else:
                         totalScore += (ord(char1) - 96)
                     lineChecked = True
+        lineCounted += 1
     openFile.close()
     print(f"the total score for part 1 is {totalScore}")
--- Days/main.py
+++ Days/main.py
@@ -29,8 +29,8 @@ sys.path.insert(0, ".\Day{:>02}".format(str(day)))
 
 # Import the first part of the day's advent challenge as "run()"
 # imputs the read file of the day as the only argument
-part1 = __import__("Day{:>02}-1".format(str(day)))
-part1.run(readInput(day))
+#part1 = __import__("Day{:>02}-1".format(str(day)))
+#part1.run(readInput(day))
 
 #Same as above except assumes part 2 is available
 part2 = __import__("Day{:>02}-2".format(str(day)))
