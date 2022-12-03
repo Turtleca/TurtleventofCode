9660a8b7ad7b1c666e27c203acdebc779d8ea98e
Turtle.ca <sleepingturtle294@gmail.com>
2022-12-03 00:17:52 -0700

    Part 1 done

--- Days/Day03/Day03-1.py
+++ Days/Day03/Day03-1.py
@@ -4,8 +4,26 @@
 # sum all up at the end
 
 def run(openFile):
+    totalScore = 0
     for line in openFile:
+        #parse the line and split into 2 equal sections
         line = line.strip("\n")
         length = len(line)
         lineList = [line[:length//2],line[length//2:]]
         print(lineList)
+
+        lineChecked = False
+        
+        #Now to check for the same character in a string
+        for char1 in lineList[0]:
+            for char2 in lineList[1]:
+                if char1 == char2 and lineChecked == False:
+                    if ord(char1) < 97:
+                        totalScore += (ord(char1) - 38)
+                        lineChecked = True
+
+                    else:
+                        totalScore += (ord(char1) - 96)
+                    lineChecked = True
+
+    print(f"the total score for part 1 is {totalScore}")
