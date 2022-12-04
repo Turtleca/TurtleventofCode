8a9434fbc7b3612b7879ec99999e5bca191c97f7
Turtle.ca <sleepingturtle294@gmail.com>
2022-12-03 23:54:31 -0700

    actually finished part 1 fucking formatting

--- Days/Day04/Day04-1.py
+++ Days/Day04/Day04-1.py
@@ -1,18 +1,17 @@
 EMPTY = ''
 def run(OpenFile):
-    elfPairs = []
     totalUselsess = 0
     for line in OpenFile:
-        line = line.split(" ")
-        line = line[0].strip("\n.")
-        if line != EMPTY:
-            elfPairs.append(line) #parse then append the stripped strings for comparison
-        if len(elfPairs) == 2:
-            # check which of the strings encomases the other
-            if elfPairs[0][0] <= elfPairs[1][0] and elfPairs[0][len(elfPairs[0])-1] >= elfPairs[1][len(elfPairs[1])-1]:
-                totalUselsess += 1
-            elif elfPairs[0][0] >= elfPairs[1][0] and elfPairs[0][len(elfPairs[0])-1] <= elfPairs[1][len(elfPairs[1])-1]:
-                totalUselsess += 1
-            elfPairs = []
+        elfPairs = line
+        elfPairs = elfPairs.strip("\n")
+        elfPairs = elfPairs.split(",")
+        elfPairs[0] = elfPairs[0].split("-")
+        elfPairs[1] = elfPairs[1].split("-") #parse then append the stripped strings for comparison
+        print(elfPairs)
+        # check which of the strings encomases the other
+        if int(elfPairs[0][0]) <= int(elfPairs[1][0]) and int(elfPairs[0][1]) >= int(elfPairs[1][1]):
+            totalUselsess += 1
+        elif int(elfPairs[0][0]) >= int(elfPairs[1][0]) and int(elfPairs[0][1]) <= int(elfPairs[1][1]):
+            totalUselsess += 1
 
-    print(totalUselsess)
+    print(f"Total overlapping sweeps are {totalUselsess}")
