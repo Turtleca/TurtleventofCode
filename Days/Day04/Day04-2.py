cbbb2eb178a3650dbf9e2a2da237525a37b9fdbb
Turtle.ca <sleepingturtle294@gmail.com>
2022-12-03 23:55:04 -0700

    hehe

--- Days/Day04/Day04-2.py
+++ Days/Day04/Day04-2.py
@@ -1,3 +1,17 @@
+EMPTY = ''
 def run(OpenFile):
+    totalUselsess = 0
     for line in OpenFile:
-        i = 1
+        elfPairs = line
+        elfPairs = elfPairs.strip("\n")
+        elfPairs = elfPairs.split(",")
+        elfPairs[0] = elfPairs[0].split("-")
+        elfPairs[1] = elfPairs[1].split("-") #parse then append the stripped strings for comparison
+        print(elfPairs)
+        # check which of the strings encomases the other
+        if int(elfPairs[0][0]) <= int(elfPairs[1][0]) and int(elfPairs[0][1]) >= int(elfPairs[1][1]):
+            totalUselsess += 1
+        elif int(elfPairs[0][0]) >= int(elfPairs[1][0]) and int(elfPairs[0][1]) <= int(elfPairs[1][1]):
+            totalUselsess += 1
+
+    print(f"Total overlapping sweeps are {totalUselsess}")
