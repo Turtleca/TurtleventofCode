a317e3fb5d266d76fb5cdf9555b18e9ec9b60946
Turtle.ca <sleepingturtle294@gmail.com>
2022-12-04 00:09:12 -0700

    done part 2 im getting faster!

--- Days/Day04/Day04-1.py
+++ Days/Day04/Day04-1.py
@@ -7,11 +7,10 @@ def run(OpenFile):
         elfPairs = elfPairs.split(",")
         elfPairs[0] = elfPairs[0].split("-")
         elfPairs[1] = elfPairs[1].split("-") #parse then append the stripped strings for comparison
-        print(elfPairs)
         # check which of the strings encomases the other
         if int(elfPairs[0][0]) <= int(elfPairs[1][0]) and int(elfPairs[0][1]) >= int(elfPairs[1][1]):
             totalUselsess += 1
         elif int(elfPairs[0][0]) >= int(elfPairs[1][0]) and int(elfPairs[0][1]) <= int(elfPairs[1][1]):
             totalUselsess += 1
 
-    print(f"Total overlapping sweeps are {totalUselsess}")
+    print(f"Total encompasing sweeps are {totalUselsess}")
--- Days/Day04/Day04-2.py
+++ Days/Day04/Day04-2.py
@@ -7,11 +7,10 @@ def run(OpenFile):
         elfPairs = elfPairs.split(",")
         elfPairs[0] = elfPairs[0].split("-")
         elfPairs[1] = elfPairs[1].split("-") #parse then append the stripped strings for comparison
-        print(elfPairs)
-        # check which of the strings encomases the other
-        if int(elfPairs[0][0]) <= int(elfPairs[1][0]) and int(elfPairs[0][1]) >= int(elfPairs[1][1]):
+        # check which of the strings overlapps at all with the other
+        if (int(elfPairs[1][0]) <= int(elfPairs[0][0]) <= int(elfPairs[1][1])) or (int(elfPairs[1][0]) <= int(elfPairs[0][1]) <= int(elfPairs[1][1])):
             totalUselsess += 1
-        elif int(elfPairs[0][0]) >= int(elfPairs[1][0]) and int(elfPairs[0][1]) <= int(elfPairs[1][1]):
+        elif (int(elfPairs[0][0]) <= int(elfPairs[1][0]) <= int(elfPairs[0][1])) or (int(elfPairs[0][0]) <= int(elfPairs[1][1]) <= int(elfPairs[0][1])):
             totalUselsess += 1
 
     print(f"Total overlapping sweeps are {totalUselsess}")
